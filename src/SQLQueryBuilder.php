a3bd88ef6a84e997739c4d4d6de2fbeb8b341bba
Mehmet Korkmaz <mehmet@mkorkmaz.com>
2016-07-01 11:29:54 +0300

    QueryBuilder refactored

--- src/SQLQueryBuilder.php
+++ src/SQLQueryBuilder.php
@@ -6,41 +6,67 @@ namespace Soupmix;
 class SQLQueryBuilder extends AbstractQueryBuilder
 {
 
+    private $queryBuilder;
+
     public function run(){
-        $this->andFilters[]= $this->orFilters;
-        $this->filters    = $this->andFilters;
-        $queryBuilder = $this->soupmix->buildQuery($this->collection, $this->filters);
-        $queryBuilderCount = clone $queryBuilder;
-        $queryBuilderCount->select(" COUNT(*) AS total ");
-        $stmt = $this->soupmix->getConnection()->executeQuery($queryBuilderCount->getSql(), $queryBuilderCount->getParameters());
-        $count = $stmt->fetchAll(\PDO::FETCH_ASSOC);
-        $numberOfSet = 0;
+        $this->queryBuilder = $this->getQueryBuilder();
+        $count = $this->getCount();
+        $numberOfRows = 0;
         if (isset($count[0]['total']) && ($count[0]['total']>0)) {
-            $numberOfSet = $count[0]['total'];
-            if ($this->sortFields !== null) {
-                $params['sort'] = '';
-                foreach ($this->sortFields as $sort_key => $sort_dir) {
-                    if ($params['sort']!='') {
-                        $params['sort'] .= ',';
-                    }
-                    $queryBuilder->addOrderBy($sort_key, $sort_dir);
-                }
-            }
-            if ($this->distinctFieldName === null) {
-                $fieldNames = ($this->fieldNames === null) ? "*" : $this->fieldNames;
-                $queryBuilder->select($fieldNames)
-                    ->setFirstResult($this->offset)
-                    ->setMaxResults($this->limit);
-            }
-            else {
-                $queryBuilder->select('DISTINCT (`' . $this->distinctFieldName . '`)');
-            }
-            $stmt = $this->soupmix->getConnection()->executeQuery($queryBuilder->getSql(), $queryBuilder->getParameters());
+            $numberOfRows = $count[0]['total'];
+            $this->setSortOrders();
+            $this->setReturnFields();
+            $this->setOffsetAndLimit();
+            $stmt = $this->soupmix->getConnection()
+                ->executeQuery(
+                    $this->queryBuilder->getSql(),
+                    $this->queryBuilder->getParameters()
+                );
             $result = $stmt->fetchAll(\PDO::FETCH_ASSOC);
             if($this->distinctFieldName !== null){
-                $numberOfSet = count($result);
+                $numberOfRows = count($result);
             }
         }
-        return ['total' => $numberOfSet, 'data' => $result];
+        return ['total' => $numberOfRows, 'data' => $result];
+    }
+
+    private function getQueryBuilder()
+    {
+        $this->andFilters[] = $this->orFilters;
+        $this->filters      = $this->andFilters;
+        return $this->soupmix->buildQuery($this->collection, $this->filters);
+    }
+
+    private function getCount()
+    {
+        $queryBuilderCount = clone $this->queryBuilder;
+        $queryBuilderCount->select(" COUNT(*) AS total ");
+        $stmt = $this->soupmix->getConnection()->executeQuery($queryBuilderCount->getSql(), $queryBuilderCount->getParameters());
+        return $stmt->fetchAll(\PDO::FETCH_ASSOC);
+    }
+
+    private function setSortOrders()
+    {
+        if ($this->sortFields !== null) {
+            foreach ($this->sortFields as $sort_key => $sort_dir) {
+                $this->queryBuilder->addOrderBy($sort_key, $sort_dir);
+            }
+        }
+    }
+
+    private function setReturnFields()
+    {
+        if ($this->distinctFieldName === null) {
+            $fieldNames = ($this->fieldNames === null) ? "*" : $this->fieldNames;
+            $this->queryBuilder->select($fieldNames);
+            return;
+        }
+        $this->queryBuilder->select('DISTINCT (`' . $this->distinctFieldName . '`)');
+    }
+
+    private function setOffsetAndLimit()
+    {
+        $this->queryBuilder->setFirstResult($this->offset)
+            ->setMaxResults($this->limit);
     }
 }
