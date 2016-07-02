17dbbcb9f71bfb725491f0a4eadb44156502f640
Mehmet Korkmaz <mehmet@mkorkmaz.com>
2016-07-02 23:38:38 +0300

    refactoring the code to apply S.R.P

--- src/SQLQueryBuilder.php
+++ src/SQLQueryBuilder.php
@@ -6,7 +6,7 @@ namespace Soupmix;
 class SQLQueryBuilder extends AbstractQueryBuilder
 {
 
-    private $queryBuilder;
+    private $queryBuilder = null;
 
     public function run(){
         $this->queryBuilder = $this->getQueryBuilder();
@@ -35,7 +35,7 @@ class SQLQueryBuilder extends AbstractQueryBuilder
             $this->andFilters[] = $this->orFilters;
         }
         $this->filters      = $this->andFilters;
-        return $this->soupmix->buildQuery($this->collection, $this->filters);
+        return $this->buildQuery($this->collection, $this->filters);
     }
 
     private function getCount()
@@ -70,4 +70,108 @@ class SQLQueryBuilder extends AbstractQueryBuilder
         $this->queryBuilder->setFirstResult($this->offset)
             ->setMaxResults($this->limit);
     }
+
+
+    protected function buildQuery($collection, $filters)
+    {
+        $queryBuilder = $this->conn->createQueryBuilder();
+        $queryBuilder->from($collection);
+        if ($filters === null) {
+            return $queryBuilder;
+        }
+        return $this->buildQueryFilters($queryBuilder, $filters);
+    }
+
+    protected function buildQueryFilters($queryBuilder, $filters)
+    {
+        foreach ($filters as $key => $value) {
+            if (strpos($key, '__') === false && is_array($value)) {
+                $queryBuilder = $this->buildQueryForOr($queryBuilder, $value);
+                continue;
+            }
+            $queryBuilder = $this->buildQueryForAnd($queryBuilder, $key, $value);
+        }
+        return $queryBuilder;
+    }
+
+    protected function buildQueryForAnd($queryBuilder, $key, $value)
+    {
+        $sqlOptions = self::buildFilter([$key => $value]);
+        if (in_array($sqlOptions['method'], ['in', 'notIn'])) {
+            $queryBuilder->andWhere(
+                $queryBuilder->expr()->{$sqlOptions['method']}( $sqlOptions['key'], $sqlOptions['value'])
+            );
+            return $queryBuilder;
+        }
+        $queryBuilder->andWhere(
+            '`'.$sqlOptions['key'].'`'
+            . ' ' . $sqlOptions['operand']
+            . ' ' . $queryBuilder->createNamedParameter($sqlOptions['value'])
+        );
+        return $queryBuilder;
+    }
+    protected function buildQueryForOr($queryBuilder, $value)
+    {
+        $orQuery =[];
+        foreach ($value as $orValue) {
+            $subKey = array_keys($orValue)[0];
+            $subValue = $orValue[$subKey];
+            $sqlOptions = self::buildFilter([$subKey => $subValue]);
+            if (in_array($sqlOptions['method'], ['in', 'notIn'])) {
+                $orQuery[] =  $queryBuilder->expr()->{$sqlOptions['method']}( $sqlOptions['key'], $sqlOptions['value']);
+                continue;
+            }
+            $orQuery[] =
+                '`'.$sqlOptions['key'].'`'
+                . ' ' . $sqlOptions['operand']
+                . ' ' . $queryBuilder->createNamedParameter($sqlOptions['value']);
+
+        }
+        $queryBuilder->andWhere(
+            '(' . implode(' OR ', $orQuery) . ')'
+        );
+        return $queryBuilder;
+    }
+
+
+    public static function buildFilter($filter)
+    {
+        $key = array_keys($filter)[0];
+        $value = $filter[$key];
+        $operator = ' = ';
+        $method = 'eq';
+        $options =[
+            'gte'       => ['method' => 'gte', 'operand' => ' >= '],
+            'gt'        => ['method' => 'gt', 'operand' => ' > '],
+            'lte'       => ['method' => 'lte', 'operand' => ' <= '],
+            'lt'        => ['method' => 'lt', 'operand' => ' < '],
+            'in'        => ['method' => 'in', 'operand' => ' IN '],
+            '!in'       => ['method' => 'notIn', 'operand' => ' NOT IN '],
+            'not'       => ['method' => 'not', 'operand' => ' NOT '],
+            'wildcard'  => ['method' => 'like', 'operand' => ' LIKE '],
+            'prefix'    => ['method' => 'like', 'operand' => ' LIKE '],
+        ];
+        if (strpos($key, '__') !== false) {
+            preg_match('/__(.*?)$/i', $key, $matches);
+            $key        = str_replace($matches[0], '', $key);
+            $queryOperator   = $matches[1];
+            $method     = $options[$queryOperator]['method'];
+            $operator   = $options[$queryOperator]['operand'];
+            switch ($queryOperator) {
+                case 'wildcard':
+                    $value = '%'.str_replace(array('?', '*'), array('_', '%'), $value).'%';
+                    break;
+                case 'prefix':
+                    $value = $value.'%';
+                    break;
+            }
+        }
+        return [
+            'key'       => $key,
+            'operand'   => $operator,
+            'method'    => $method,
+            'value'     => $value
+        ];
+    }
+
 }
