e3699ced09f17faa16ea62b09a9696ee8f779e5f
Mehmet Korkmaz <mehmet@mkorkmaz.com>
2016-09-10 03:52:20 +0300

    dbal ver upgraded

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,8 +2,8 @@
 namespace tests;
 
 use Composer\Autoload\ClassLoader;
-
 require_once dirname(__DIR__) . '/vendor/autoload.php';
+
 $loader = new ClassLoader();
 $loader->add('tests', dirname(__DIR__));
 $loader->register();
